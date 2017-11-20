6b865917226e412bf48fabce77290c0e96c34b06
David von Kiedrowski <dvonkiedrowski@hotmail.de>
2017-11-20 10:44:03 +0100

    layout fixes

--- Zoll/Scripts/app/module.js
+++ Zoll/Scripts/app/module.js
@@ -1,9 +1,16 @@
 ﻿(function () {
     'use strict';
 
-    angular.module('app', ['ui.bootstrap', 'ngFabForm', 'ngMessages', 'dataGrid', 'pagination']).config(function (ngFabFormProvider) {
-        ngFabFormProvider.extendConfig({
-            validationsTemplate: '../../Templates/germanValidationTemplate.html'
+    angular.module('app', ['ui.bootstrap', 'ngFabForm', 'ngMessages', 'dataGrid', 'pagination'])
+        .config(function (ngFabFormProvider) {
+            ngFabFormProvider.extendConfig({
+                validationsTemplate: '../../Templates/germanValidationTemplate.html'
+            });
+        }).run(function (paginationConfig) {
+            paginationConfig.firstText = 'Erste';
+            paginationConfig.previousText = 'Vorherige';
+            paginationConfig.nextText = 'Nächste';
+            paginationConfig.lastText = 'Letzte';
+
         });
-    });
 })();
